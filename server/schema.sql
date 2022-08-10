784f903406cff5a709d95f76b4e8d3db0adadc5d
Matt Dailey <mattdailey@matts-mbp.lan>
2022-08-09 21:00:24 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -2,16 +2,42 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
 
 /* Create other tables and define schemas for them here! */
+CREATE TABLE users (
+  id integer primary key,
+  name text NOT NULL
+);
+
+CREATE TABLE rooms (
+  id integer primary key,
+  name text NOT NULL,
+  -- message_id integer
+  -- FOREIGN KEY(message_id) REFERENCES messages(id)
+);
 
 
-
+CREATE TABLE messages (
+  id integer primary key,
+  user_id integer,
+  usertext text NOT null,
+  room_id integer,
+  FOREIGN KEY(user_id) REFERENCES users(id),
+  FOREIGN KEY(room_id) REFERENCES rooms(id)
+);
 
 /*  Execute this file from the command line by typing:
  *    mysql -u root < server/schema.sql
  *  to create the database and the tables.*/
 
+-- INSERT INTO table1
+--        VALUES (2,2205,2022);
+--        mysql Show schemas;
+
+-- In MySQL, physically, a schema is synonymous with a database. You can substitute the keyword SCHEMA instead of DATABASE in MySQL SQL syntax, for example using CREATE SCHEMA instead of CREATE DATABASE.
+
+
+-- foreign key(class_id) references classes(id),
+
+-- SHOW {DATABASES | SCHEMAS}
+--     [LIKE 'pattern' | WHERE expr]
